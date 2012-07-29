665d9a3d18330500fceda4d3d6571303cef9a4be
tommykins20 <tommymyers1201@googlemail.com>
2012-07-29 14:10:13 +0100

    Updated main class

--- IRChat/src/me/tommykins20/IRChat/IRChat.java
+++ IRChat/src/me/tommykins20/IRChat/IRChat.java
@@ -26,6 +26,8 @@ public class IRChat extends JavaPlugin {
 		{
 			try {
 				(new File(absfile+File.separator+"config.yml")).createNewFile();
+				logMessage("This appears to be your first time using IRChat (or you deleted your config)! You can customise your config.yml in the IRChat" +
+						"folder in the plugins directory to your likings.");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -36,6 +38,7 @@ public class IRChat extends JavaPlugin {
 		try {
 			bot.connect(getIRCServer(), getIRCPort());
 			bot.joinChannel(getIRCChannel());
+			bot.getListenerManager().addListener(new IRChatBotListener(this));
 			logMessage("Successfully joined IRC server "+getIRCServer()+":"+getIRCPort()+" on channel "+getIRCChannel()+" using nickname "+getIRCUsername());
 		} catch (Exception e) {
 			logWarnMessage("Error when attempting to join IRC server...");
@@ -49,59 +52,99 @@ public class IRChat extends JavaPlugin {
 		logMessage("Disconnected from current IRC server.");
 	}
 	
+	/**
+	 * Logs a message to the server console with this format "[IRChat] <message>"
+	 * @param msg - Message to log
+	 */
 	public void logMessage(String msg)
 	{
 		logger.info("[IRChat] "+msg);
 	}
 	
+	/**
+	 * Logs a warning message to the server console with this format "[IRChat] <message>"
+	 * @param msg - Message to log
+	 */
 	public void logWarnMessage(String msg)
 	{
 		logger.warning("[IRChat] "+msg);
 	}
 	
+	/**
+	 * Saves the config file
+	 */
 	@Override
 	public void saveDefaultConfig()
 	{
 		super.saveDefaultConfig();
 		getConfig().set("yourservername", "default");
 		getConfig().set("username", "IRChatBot");
-		getConfig().set("server", "irc.esper.net");
+		getConfig().set("server", "irc.freenode.org");
 		getConfig().set("port", 6667);
-		getConfig().set("channel", "#bukkit");
+		getConfig().set("channel", "#pircbotx");
 		getConfig().set("chatformat", "&2[IRC] &e%name%&f: %msg%");
 		saveConfig();
 	}
 	
+	/**
+	 * Gets the server name the user specified
+	 * @return Server name for IRC
+	 */
 	public String getServerName()
 	{
 		return getConfig().getString("yourservername");
 	}
 	
+	/**
+	 * Gets the username used for logging into IRC servers
+	 * @return Username from config.yml
+	 */
 	public String getIRCUsername()
 	{
 		return getConfig().getString("username");
 	}
 	
+	/** 
+	 * Gets the IRC server to connect to
+	 * @return IRC Server ip to connect to
+	 */
 	public String getIRCServer()
 	{
 		return getConfig().getString("server");
 	}
 	
+	/**
+	 * Gets the IRC Server port to connect to
+	 * @return IRC Port to connect
+	 */
 	public int getIRCPort()
 	{
 		return getConfig().getInt("port");
 	}
 	
+	/**
+	 * Gets IRC channel to connect to
+	 * @return IRC channwl to connect to
+	 */
 	public String getIRCChannel()
 	{
 		return getConfig().getString("channel");
 	}
 	
+	/**
+	 * Gets the specified chat format to broadcast IRC messages to players
+	 * @return Specified chat format from config.yml
+	 */
 	public String getChatFormat()
 	{
 		return getConfig().getString("chatformat");
 	}
 	
+	/**
+	 * Broadcasts IRC message to server and log to console
+	 * @param sender - Message sender
+	 * @param message - Actual message
+	 */
 	public void broadcastIRCMessage(String sender, String message)
 	{
 		for(Player online : getServer().getOnlinePlayers())
