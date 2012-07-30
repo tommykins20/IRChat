47e74daea3762de0d534c8ee5297e48daadcaa30
tommykins20 <tommymyers1201@googlemail.com>
2012-07-30 10:58:41 +0100

    Added a couple new events

--- IRChat/src/me/tommykins20/IRChat/IRChatBotListener.java
+++ IRChat/src/me/tommykins20/IRChat/IRChatBotListener.java
@@ -1,7 +1,9 @@
 package me.tommykins20.IRChat;
 
 import org.pircbotx.hooks.*;
+import org.pircbotx.hooks.events.JoinEvent;
 import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.events.QuitEvent;
 
 @SuppressWarnings("rawtypes")
 public class IRChatBotListener extends ListenerAdapter implements Listener {
@@ -19,6 +21,23 @@ public class IRChatBotListener extends ListenerAdapter implements Listener {
 		String message = event.getMessage();
 		String senderName = event.getUser().getNick();
 		plugin.broadcastIRCMessage(senderName, message);
+	}
+	
+	@Override
+	public void onEvent(Event event) throws Exception
+	{
+		if(event instanceof JoinEvent)
+		{
+			JoinEvent je = (JoinEvent) event;
+			String playername = je.getUser().getNick();
+			plugin.broadcastDifferentIRCMessage(playername+" has joined the IRC channel.");
+		}else
+			if(event instanceof QuitEvent)
+			{
+				QuitEvent qe = (QuitEvent) event;
+				String playername = qe.getUser().getNick();
+				plugin.broadcastDifferentIRCMessage(playername+" has left the IRC channel.");
+			}
 	}
 
 }
